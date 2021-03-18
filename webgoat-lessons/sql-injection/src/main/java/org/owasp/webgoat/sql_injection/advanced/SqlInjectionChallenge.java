537d411dfe09cf4b358f1bd6e90330f4c10a7592
r3m3n00 <73182530+r3m3n00@users.noreply.github.com>
2021-03-18 19:26:46 +0100

    Update SqlInjectionChallenge Try 2

--- webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/advanced/SqlInjectionChallenge.java
+++ webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/advanced/SqlInjectionChallenge.java
@@ -62,7 +62,9 @@ public class SqlInjectionChallenge extends AssignmentEndpoint {
 
         if (attackResult == null) {
             try (Connection connection = dataSource.getConnection()) {
-                String checkUserQuery = "select userid from sql_challenge_users where userid = '" + username_reg + "'";
+                String checkUserQuery = "select userid from sql_challenge_users where userid = ?";
+                PreparedStatement query = connection.prepareStatement(checkUserQuery);
+                query.setString(1,username_reg);
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(checkUserQuery);
 
